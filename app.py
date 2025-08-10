def71e47ccd60c4360735f99a071f3114d6fbf4e
Alex-Vicente11 <teiker_01@hotmail.com>
2025-08-09 23:43:03 -0600

    eliminar código innecesario

--- app.py
+++ app.py
@@ -357,11 +357,6 @@ def obtener_productos():
         return jsonify({'error': str(e)}), 500
 
 
-
-
-
-
-
 @app.route('/home') 
 def home():
     try: 
@@ -467,54 +462,6 @@ def add_product_web():
                                  message=f'Error al agregar producto: {str(e)}', #aqui condicionar
                                  success=False)
 
-
-@app.route('/product')
-def product():
-    return get_html_base("""
-    <div class="container-md" style="background-color: cyan;"> 
-        <div class="row">
-            <div class="col-4 ">
-                <img src="https://3.bp.blogspot.com/-MfvUawjHslc/U4tvUsR9hbI/AAAAAAACNoQ/ALk3_7AMSbk/s1600/imagenes-gratis-fondos-fotos-y-postales-para-computadoras-dise%C3%B1adores-publicistas-y-estudiantes-free-photos+(8).jpg" class="img-fluid" alt="inversor" >
-            </div>
-        </div>
-
-        <div class="row">
-            <div class="col-4 mt-4 p-5  border border-5 border-primary" style="background-color: orange">
-                <h1>Inversor trifásico bidireccional para motores asíncronos</h1>
-                <p>Cuenta con disipador  de calor, etapa de potencia, sensores de corriente y temperatura.</p>
-            </div>
-        </div>
-
-        <div class="row">
-            <div class="col-4 md-4" style="background-color: gray;">
-                <h2>$ 1,300</h2>
-                <p>Inversor modificado 3000W de onda sinusoidal DC 12v a AC 110V</p>
-            </div>
-        </div>
-     </div>                  
-    """)
-
-#<div class="col-12 mt-4 p-5  border border-5 border-primary" style="background-color: orange">
-#Dentro de estas funciones se puede implementar más código para tener una ejeción adecuada
-#Para realizar acciones antes de una petición
-@app.before_request
-def before_request():
-    print("Antes de la petición...")
-
-#Para realizar acciones después de la petición
-@app.after_request
-def after_request(respose):  #se requiere para despues de la vista el parametro response
-    print("Después de la petición")
-    return respose
-
-#query_string significa pasarle a una url un serie de parametros que puede ser variable
-def query_string():  #se considera una vista?
-    print(request)   #es un objeto (sera lo que un cliente solicita a un servidor)
-    print(request.args)
-    print(request.args.get('param1')) #param1 es nombre de la llave y envía el parametro
-    print(request.args.get('param2')) 
-    return "OK"      #necesariamente se debe retornar algo
-#en la pag web, ? indica que se pasan parametros (param1=jose)
 
 # Vista del carrito de compras
 @app.route('/cart')
@@ -630,10 +577,5 @@ def remove_from_cart():
 
 
 if __name__=='__main__': #se comprueba la aplicacion
-    app.add_url_rule('/query_string',view_func=query_string) #aqui se enlaza la funcion a la url
-#   app.register_error_handler(404, pagina_no_encontrada)    #se registra el manejador de error que apunte a la funcion pag_no_encontrada
     app.run(debug=True, port=5002)  #aqui se corre el programa
 
-#observar el estado de Debug mode: ctrl + c detengo el servidor
-#activar el modo depuracion, dentro de run(debug=true), aquí puedo determinar el puerto para la ejecucion
-#plantilla: documento html que tiene contenido que es devuelto a traves del servidor 
